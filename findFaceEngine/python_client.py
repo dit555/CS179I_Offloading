14f0d15e62c75de78dff22f470734bbe9e63df2a
dit55 <dit55@localhost>
2021-05-27 16:25:44 -0400

    node to node transfer of data

--- findFaceEngine/python_client.py
+++ findFaceEngine/python_client.py
@@ -2,12 +2,13 @@
 
 import socket
 
-HOST = '198.72.184.1'  # The server's hostname or IP address
+HOST = '130.127.134.18'  # The server's hostname or IP address
 PORT = 65431        # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    s.sendall(b'Start DL traffic')
-    #data = s.recv(1024)
+    sii = "lkajsd"
+    s.sendall(str.encode(sii))
+    data = s.recv(1024)
 
-#print('Received', repr(data))
+print(sii, repr(data))
