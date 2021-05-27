0f304d4ab4a74a4a48308e857900562e3d667466
Dumitru Chiriac <56750709+dit555@users.noreply.github.com>
2021-05-27 12:35:50 -0700

    added servers to local machine

---
+++ findFaceEngine/python_server.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import socket, os
+
+HOST = '76.82.39.133'  # Standard loopback interface address (localhost)
+PORT = 65431        # Port to listen on (non-privileged ports are > 1023)
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+	s.bind((HOST, PORT))
+	s.listen()
+	conn, addr = s.accept()
+	with conn:
+		print('Connected by', addr)
+		while True:
+			data = conn.recv(1024)
+			if not data:
+				break
+			#os.system("../MoonGen/build/MoonGen ../MoonGen/examples/pcap/replay-pcap.lua 0 ../test-packet/gtp_icmp_echo_request_256_loop.pcap -l")
+			#conn.sendall(data)
+			print(data)
