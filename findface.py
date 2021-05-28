43c3bd7b1fb146137b251e1a957a9e37cf56ef3c
Dumitru Chiriac <56750709+dit555@users.noreply.github.com>
2021-05-27 19:28:12 -0700

    Update findface.py

--- findface.py
+++ findface.py
@@ -2,51 +2,61 @@ import cv2
 import sys
 import logging as log
 import datetime as dt
+import numpy as np
 from time import sleep
 
 cascPath = "data.xml"
 faceCascade = cv2.CascadeClassifier(cascPath)
-log.basicConfig(filename='webcam.log',level=log.INFO)
 
+overlay_path = "me_gusta_filter.png"
 video_capture = cv2.VideoCapture(0)
 anterior = 0
 
 while True:
-    if not video_capture.isOpened():
-        print('Unable to load camera.')
-        sleep(5)
-        pass
+	if not video_capture.isOpened():
+		print('Unable to load camera.')
+		sleep(5)
+		pass
 
-    # Capture frame-by-frame
-    ret, frame = video_capture.read()
+	# Capture frame-by-frame
+	ret, frame = video_capture.read()
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    faces = faceCascade.detectMultiScale(
-        gray,
-        scaleFactor=1.1,
-        minNeighbors=5,
-        minSize=(30, 30)
-    )
+	faces = faceCascade.detectMultiScale(
+		gray,
+		scaleFactor=1.1,
+		minNeighbors=5,
+		minSize=(30, 30)
+	)
+	
+	overlay = cv2.imread(overlay_path)
+	# Draw a rectangle around the faces
+	for (x, y, w, h) in faces:
+		center_x = int(x + w/2)
+		center_y = int(y + h/2)
+		x2 = int(overlay.shape[1]/2)
+		y2 = int(overlay.shape[0]/2)
+		y_1 = center_y - y2
+		y_2 = center_y + y2
+		x_1 = center_x - x2
+		x_2 = center_x + x2
+		frame[y_1:y_2, x_1:x_2] = overlay
+		
 
-    # Draw a rectangle around the faces
-    for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-
-    if anterior != len(faces):
-        anterior = len(faces)
-        log.info("faces: "+str(len(faces))+" at "+str(dt.datetime.now()))
+	if anterior != len(faces):
+		anterior = len(faces)
 
 
-    # Display the resulting frame
-    cv2.imshow('Video', frame)
+	# Display the resulting frame
+	cv2.imshow('Video', frame)
 
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
 
-    # Display the resulting frame
-    cv2.imshow('Video', frame)
+	# Display the resulting frame
+	cv2.imshow('Video', frame)
 
 # When everything is done, release the capture
 video_capture.release()
